3bb72cd6eedc9a36881b571d31d6c5b032d48295
Conseque <conseque@gmail.com>
2012-04-23 11:34:21 +0400

    modified code

--- askr.pl
+++ askr.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/perl
-
-
+use Redis::hiredis; my $r=Redis::hiredis->new(host=>"i",port=>6380); $r->select(15);			# db to collect
+$bestbefore=40000;	$expired=time-$bestbefore;	print time, "\texpiration : $expired\n"; my $OUT;
+@hstos=@{$r->zrangebyscore(":IP:", '-inf', $expired)};	print time, "\texpired: ", $#hstos+1, "hosts\n";
+							exit if $#hstos<0;				# quit if no expired hstos
+							my $hsto=$hstos[0];				# oldest hsto is to be processd
+							$r->zadd(":IP:", time, $hsto);			# set processing freshness
+							print time, "\tprocessing $hsto...\n";		#
 use Switch; my $ssh='SSH_AUTH_SOCK=/tmp/ssh-zhNBuE6383/agent.6383 ssh -o ConnectTimeout=2 root@';	# ssh auth
-use Redis::hiredis; my $r = Redis::hiredis->new(host=>"i",port=>6380);					# redis connection
-$r->select(5); my $hsto = @{$r->zrange(":IP:",-1,-1)}[0]; $r->select(15); my $OUT;			# host to connect
 use IPC::Open2;	local (*O,*I); my $pid=open2(*O,*I,"${ssh}${hsto}");					# ssh connection
 &ask ("cat /etc/issue"); $r->set("$hsto:issue", $OUT); switch ($OUT) {	case /ubuntu/i	{ &ubun() }	# U for ubuntu
 									case /red hat/i	{ &redh() }	# R for rhel
@@ -12,10 +15,13 @@ use IPC::Open2;	local (*O,*I); my $pid=open2(*O,*I,"${ssh}${hsto}");					# ssh c
 									case /suse/i	{ &sles() }	# S for sles
 									case /solar/i	{ &sola() }	# O for solaris
 									else		{ &othr() } }	# F for other stuff
-			push @cmnds, @{$r->smembers("!C")};	push @files, @{$r->smembers("!c")};	# C for common unix
-			for (@cmnds) { &ask("$_");	s/ //g;		$r->set("$hsto:!$_", $OUT); }	# execute @cmnds
-			for (@files) { &ask("cat $_");	s/.*\///g;	$r->set("$hsto:$_", $OUT); }	# read all @files
-			print I "exit\n";				1 while (<O>);			# exit ssh
+		push @cmnds, @{$r->smembers("!C")};		push @files, @{$r->smembers("!c")};	# C for common unix
+		for (@cmnds) { &ask("$_");	s/ //g;		$r->set("$hsto:!$_", $OUT);		# exec all cmnds
+								$r->zadd(":$hsto:", time, "!$_"); }	# set cmnds freshness
+		for (@files) { &ask("cat $_");	s/.*\///g;	$r->set("$hsto:$_", $OUT);		# read all files
+								$r->zadd(":$hsto:", time, $_); }	# set files freshness
+		$r->set($hsto,$hsto);	$r->expire($hsto, $bestbefore);					# set hsto freshness
+		print I "exit\n";	1 while (<O>);							# exit ssh
 #####################################################################################################KEEPWATCH!
 sub ask() { $OUT=''; print I "$_[0]\necho ===\n"; while (($l=<O>) !~ m/===/) { $OUT .= $l }; $OUT=~s/\n$//g; }
 ###############################################################################################################
